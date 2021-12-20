eefda9af9407033e680bcd7867c4078798ce7467
dapr1nc3 <ddaprincem84@gmail.com>
2021-12-20 15:50:49 -0500

    stored player objects giving the player properties related to user character

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -18,6 +18,28 @@ test('creates a player object', () => {
       );
 });
 
+// test function for new player with status, ere, we're checking that player.getStats() returns an object with four specific properties.
+test("gets players stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+
+//  A second test for the next method:
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
+
 // NOTES on .test files and running test
 // When you run npm run test, it will run all files within the __tests__ directory and any other .test files throughout the project.
 //  If you only want to run a particular group of related tests (known as a test suite), you can modify the npm run command as shown here:
