93166a22753fe2e4d0343e25f984e11f2d09d39f
Devin Minnis <ddaprincem84@gmail.com>
2021-12-21 18:05:39 -0500

    "merge commit"

---
+++ __tests__/Player.test.js
@@ -0,0 +1,108 @@
+// require statements
+const Player = require('../lib/Player');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+
+// test function for health, strength, agility
+test('creates a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+      );
+});
+
+// test function for new player with status, ere, we're checking that player.getStats() returns an object with four specific properties.
+test("gets players stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+
+//  A second test for the next method:
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
+
+//  A test to get information about the player's health
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+//  Method to check if the player is alive.
+test('checks if player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+// Method to see if the correct amount of health is being subtracted from the Player
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
+});
+
+//  A new test that verifies that a player's attack value is within range:
+test("get's player's attack value", () => {
+    const player = new Player('Dave');
+    player.strength = 10;
+
+    expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+// Method that adds a potion to the players inventory
+test('adds a potion to the inventory', () => {
+    const player = new Player('Dave');
+    const oldCourt = player.inventory.length;
+
+    player.addPotion(new Potion());
+
+    expect(player.inventory.length).toBeGreaterThan(oldCourt);
+});
+
+// uses a potion from the players inventory 
+test('uses a potion from inventory', () => {
+    const player = new Player('Dave');
+    player.inventory = [new Potion(), new Potion(), new Potion()];
+    const oldCount = player.inventory.length;
+
+    player.usePotion(1);
+
+    expect(player.inventory.length).toBeLessThan(oldCount);
+});
+
+// NOTES on .test files and running test
+// When you run npm run test, it will run all files within the __tests__ directory and any other .test files throughout the project.
+//  If you only want to run a particular group of related tests (known as a test suite), you can modify the npm run command as shown here:
+// npm run test {name of .test file}
